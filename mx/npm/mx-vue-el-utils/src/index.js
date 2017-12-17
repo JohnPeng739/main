e947fd09571164b6e7c18226447c0a3f70b57845
John Peng <josh_73_9@hotmail.com>
2017-12-17 17:02:27 +0800

    添加了支持Material Icon的icon组件。

--- mx/npm/mx-vue-el-utils/src/index.js
+++ mx/npm/mx-vue-el-utils/src/index.js
@@ -2,7 +2,9 @@ import ajax from './utils/ajax'
 import notify from './utils/notify'
 import formValidateRules from './utils/form-validate-rules'
 
+import Icon from './components/icon.vue'
+
 import TagNormal from './components/form/tag-normal.vue'
 import TagCouple from './components/form/tag-couple.vue'
 
-export {ajax, notify, formValidateRules, TagNormal, TagCouple}
+export {ajax, notify, Icon, formValidateRules, TagNormal, TagCouple}
--- mx/npm/mx-vue-el-utils/src/sample/pages/home.vue
+++ mx/npm/mx-vue-el-utils/src/sample/pages/home.vue
@@ -2,15 +2,24 @@
   <div>
     <h1>测试首页</h1>
     <img src="../assets/logo.png">
-    <br/>
+    <br/><br/>
+    some material icon:
+    <icon></icon>
+    <icon name="home"></icon>
+    <icon name="build"></icon>
+    <icon name="3d_rotation"></icon>
+    <br/><br/>
     <el-button @click="handleGoto('/notify')">Notify</el-button>
     <el-button @click="handleGoto('/tag')">Tag</el-button>
   </div>
 </template>
 
 <script>
+  import Icon from '@/components/icon'
+
   export default {
     name: 'home-page',
+    components: {Icon},
     data () {
       return {}
     },
