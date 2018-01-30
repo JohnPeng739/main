a70ab14df5b3b84e29ff098f1e00c9de641b1c94
John Peng <josh_73_9@hotmail.com>
2018-01-30 12:11:46 +0800

    修复了没有定义初始化任务时导致的错误。

--- mx/core/mx-spring/src/main/java/org/mx/spring/InitializeTaskFactory.java
+++ mx/core/mx-spring/src/main/java/org/mx/spring/InitializeTaskFactory.java
@@ -22,50 +22,52 @@ public class InitializeTaskFactory {
     private ApplicationContext context = null;
 
     public void init() {
-        List<Class<InitializeTask>> tasks = (List<Class<InitializeTask>>) context.getBean(TASKS, List.class);
-        if (tasks != null && !tasks.isEmpty()) {
-            List<InitializeTask> shortTimes = new ArrayList<>();
-            List<Callable<Void>> longTimes = new ArrayList<>();
-            tasks.forEach(taskClass -> {
-                try {
-                    final InitializeTask task = taskClass.newInstance();
-                    if (task.isLongTimeTask()) {
-                        longTimes.add(new Callable<Void>() {
-                            @Override
-                            public Void call() throws Exception {
-                                task.invokeTask();
-                                return null;
-                            }
-                        });
-                    } else {
-                        shortTimes.add(task);
-                    }
-                } catch (InstantiationException | IllegalAccessException ex) {
-                    if (logger.isErrorEnabled()) {
-                        logger.error(String.format("Initialize the task[%s] fail.", taskClass.getSimpleName()));
-                    }
-                }
-            });
-            try {
-                service = Executors.newCachedThreadPool();
-                longTimes.add(new Callable<Void>() {
-                    @Override
-                    public Void call() throws Exception {
-                        shortTimes.forEach(task -> task.invokeTask());
-                        return null;
+        if (context.containsBean(TASKS)) {
+            List<Class<InitializeTask>> tasks = (List<Class<InitializeTask>>) context.getBean(TASKS, List.class);
+            if (tasks != null && !tasks.isEmpty()) {
+                List<InitializeTask> shortTimes = new ArrayList<>();
+                List<Callable<Void>> longTimes = new ArrayList<>();
+                tasks.forEach(taskClass -> {
+                    try {
+                        final InitializeTask task = taskClass.newInstance();
+                        if (task.isLongTimeTask()) {
+                            longTimes.add(new Callable<Void>() {
+                                @Override
+                                public Void call() throws Exception {
+                                    task.invokeTask();
+                                    return null;
+                                }
+                            });
+                        } else {
+                            shortTimes.add(task);
+                        }
+                    } catch (InstantiationException | IllegalAccessException ex) {
+                        if (logger.isErrorEnabled()) {
+                            logger.error(String.format("Initialize the task[%s] fail.", taskClass.getSimpleName()));
+                        }
                     }
                 });
-                service.invokeAll(longTimes);
-            } catch (InterruptedException ex) {
-                if (logger.isErrorEnabled()) {
-                    logger.error("Invoke task fail.", ex);
+                try {
+                    service = Executors.newCachedThreadPool();
+                    longTimes.add(new Callable<Void>() {
+                        @Override
+                        public Void call() throws Exception {
+                            shortTimes.forEach(task -> task.invokeTask());
+                            return null;
+                        }
+                    });
+                    service.invokeAll(longTimes);
+                } catch (InterruptedException ex) {
+                    if (logger.isErrorEnabled()) {
+                        logger.error("Invoke task fail.", ex);
+                    }
                 }
             }
         }
     }
 
     public void close() {
-        if (service !=  null) {
+        if (service != null) {
             service.shutdownNow();
             service = null;
         }
