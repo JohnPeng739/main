0f404e7a512d348819efb973f1d5fd54b13af77f
John Peng <josh_73_9@hotmail.com>
2018-02-13 13:04:05 +0800

    修改了tag中存在性匹配中的bug。

--- mx/npm/mx-vue-el-utils/src/components/mx-choose-tag/mx-choose-tag.vue
+++ mx/npm/mx-vue-el-utils/src/components/mx-choose-tag/mx-choose-tag.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <el-tag class="tag" v-for="item in tags" :key="item.id" :closable="!disabled" :close-transition="false"
-            :type="type" @close="handleDeleteTag(item)">{{getDisplayName(item)}}</el-tag>
+            :type="type" @close="handleDeleteTag(item)">{{getDisplayName(item)}}
+    </el-tag>
     <el-popover ref="popover" v-model="visible" placement="bottom" :width="popoverWidth" trigger="click">
       <el-row type="flex">
         <el-col :span="24">
@@ -22,7 +23,7 @@
 </template>
 
 <script>
-  import {formatter} from 'mx-app-utils'
+  import { formatter } from 'mx-app-utils'
   import MxNotify from '../../utils/mx-notify'
 
   export default {
@@ -32,7 +33,11 @@
       displayFormat: String,
       disabled: {type: Boolean, default: false},
       type: String,
-      popoverWidth: {type: Number, default: 400}
+      popoverWidth: {type: Number, default: 400},
+      keyField: {
+        type: String,
+        default: 'code'
+      }
     },
     data () {
       return {
@@ -40,8 +45,13 @@
       }
     },
     computed: {
-      tags () {
-        return this.value
+      tags: {
+        get () {
+          return this.value
+        },
+        set (val) {
+          this.$emit('input', val)
+        }
       }
     },
     methods: {
@@ -63,13 +73,18 @@
           if (tags.length > 0) {
             for (let index in tags) {
               let tag = tags[index]
-              if (this.tags.indexOf(tag) >= 0) {
-                MxNotify.warn(this.$t('message.tag.existed', {tag: this.getDisplayName(tag)}))
+              if (!tag[this.keyField]) {
+                MxNotify.error(this.$t('message.tag.fieldNotExist', [this.keyField]))
                 return
-              } else {
-                this.tags.push(tag)
+              }
+              for (let innerIndex in this.tags) {
+                if (this.tags[innerIndex][this.keyField] === tag[this.keyField]) {
+                  MxNotify.warn(this.$t('message.tag.existed', {tag: this.getDisplayName(tag)}))
+                  return
+                }
               }
             }
+            this.tags = this.tags.concat(tags)
             this.visible = false
             this.$emit('change', this.tags)
           } else {
