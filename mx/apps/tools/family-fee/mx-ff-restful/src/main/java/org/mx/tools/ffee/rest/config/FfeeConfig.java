65969ca54fbfab65331540183a51176c7293d3e4
John Peng <josh_73_9@hotmail.com>
2018-02-20 10:42:24 +0800

    基本完成了账户管理的常规账户注册服务。

--- mx/apps/tools/family-fee/mx-ff-restful/src/main/java/org/mx/tools/ffee/rest/config/FfeeConfig.java
+++ mx/apps/tools/family-fee/mx-ff-restful/src/main/java/org/mx/tools/ffee/rest/config/FfeeConfig.java
@@ -1,12 +1,16 @@
 package org.mx.tools.ffee.rest.config;
 
-import org.mx.comps.rbac.RBACRestApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
+import org.mx.comps.rbac.rest.config.RbacRestConfig;
+import org.mx.comps.rbac.rest.tasks.InitializeAdminAccountTask;
+import org.mx.spring.InitializeTask;
+import org.mx.tools.ffee.rest.FfeeAccountManageResource;
+import org.mx.tools.ffee.rest.FfeeFamilyManageResource;
+import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 描述： FFEE应用中Restful服务配置类。
@@ -17,11 +21,20 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAspectJAutoProxy
 @EnableJpaRepositories(basePackages = "org.mx.tools.ffee.repository")
 @EnableTransactionManagement
-@Import({RBACRestApplication.class})
+@Import({RbacRestConfig.class})
 @PropertySource({"classpath:ffee.properties"})
 @ComponentScan({
         "org.mx.tools.ffee.restful",
         "org.mx.tools.ffee.service.impl"
 })
 public class FfeeConfig {
+    @Bean(name = "initializeTasks")
+    public List<Class<? extends InitializeTask>> initializeTasks() {
+        return Arrays.asList(InitializeAdminAccountTask.class);
+    }
+
+    @Bean(name = "restfulClassesFFEE")
+    public List<Class<?>> ffeeRestfulClasses() {
+        return Arrays.asList(FfeeAccountManageResource.class, FfeeFamilyManageResource.class);
+    }
 }
