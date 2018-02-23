87c5e2c2127d41384fec48e9ac68ec2a108c80e5
John Peng <josh_73_9@hotmail.com>
2018-02-23 18:45:17 +0800

    修改Course字典为树状字典。

--- mx/apps/tools/family-fee/mx-ff-service-hibernate/src/main/java/org/mx/tools/ffee/dal/entity/CourseEntity.java
+++ mx/apps/tools/family-fee/mx-ff-service-hibernate/src/main/java/org/mx/tools/ffee/dal/entity/CourseEntity.java
@@ -1,6 +1,6 @@
 package org.mx.tools.ffee.dal.entity;
 
-import org.mx.dal.entity.BaseEntity;
+import org.mx.dal.entity.BaseDictTreeEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,55 +10,15 @@ import javax.persistence.Table;
  * 描述： 科目明细信息实体类，基于Hibernate实现。
  *
  * @author John.Peng
- * Date time 2018/2/17 下午5:30
+ *         Date time 2018/2/17 下午5:30
  */
 @Entity
 @Table(name = "TB_COURSE")
-public class CourseEntity extends BaseEntity implements Course {
-    @Column(name = "NAME", length = 50)
-    private String name;
-    @Column(name = "DESCRIPTION")
-    private String desc;
+public class CourseEntity extends BaseDictTreeEntity implements Course {
     @Column(name = "COURSE_TYPE")
     private BudgetItem.Type type;
     @Column(name = "IS_PUBLIC")
     private boolean isPublic;
-
-    /**
-     * {@inheritDoc}
-     *
-     * @see Course#getName()
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @see Course#setName(String)
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @see Course#getDesc()
-     */
-    public String getDesc() {
-        return desc;
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @see Course#setDesc(String)
-     */
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
 
     /**
      * {@inheritDoc}
--- mx/apps/tools/family-fee/mx-ff-service/src/main/java/org/mx/tools/ffee/dal/entity/Course.java
+++ mx/apps/tools/family-fee/mx-ff-service/src/main/java/org/mx/tools/ffee/dal/entity/Course.java
@@ -1,22 +1,14 @@
 package org.mx.tools.ffee.dal.entity;
 
-import org.mx.dal.entity.Base;
+import org.mx.dal.entity.BaseDictTree;
 
 /**
- * 描述： 科目类型信息定义
+ * 描述： 科目类型信息定义，科目为树状字典。
  *
  * @author John.Peng
  *         Date time 2018/2/17 下午4:46
  */
-public interface Course extends Base {
-    String getName();
-
-    void setName(String name);
-
-    String getDesc();
-
-    void setDesc(String desc);
-
+public interface Course extends BaseDictTree {
     BudgetItem.Type getType();
 
     void setType(BudgetItem.Type type);
