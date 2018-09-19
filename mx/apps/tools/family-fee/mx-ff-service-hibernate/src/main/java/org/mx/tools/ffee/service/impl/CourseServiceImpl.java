984efd16e006de5809637e11c5175f7a577a994d
John Peng <josh_73_9@hotmail.com>
2018-09-19 20:45:47 +0800

    fix(mx-ff): 修改了科目保存发生错误的问题，更新了设计文档。

--- mx/apps/tools/family-fee/mx-ff-service-hibernate/src/main/java/org/mx/tools/ffee/service/impl/CourseServiceImpl.java
+++ mx/apps/tools/family-fee/mx-ff-service-hibernate/src/main/java/org/mx/tools/ffee/service/impl/CourseServiceImpl.java
@@ -6,6 +6,7 @@ import org.mx.StringUtils;
 import org.mx.dal.service.GeneralDictAccessor;
 import org.mx.error.UserInterfaceSystemErrorException;
 import org.mx.tools.ffee.dal.entity.Course;
+import org.mx.tools.ffee.dal.entity.FfeeAccount;
 import org.mx.tools.ffee.error.UserInterfaceFfeeErrorException;
 import org.mx.tools.ffee.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,11 +58,22 @@ public class CourseServiceImpl implements CourseService {
                     UserInterfaceSystemErrorException.SystemErrors.SYSTEM_ILLEGAL_PARAM
             );
         }
-        if (!StringUtils.isBlank(course.getOwner().getId()))
+        FfeeAccount owner = null;
+        if (course.getOwner() != null && !StringUtils.isBlank(course.getOwner().getId())) {
+            owner = generalDictAccessor.getById(course.getOwner().getId(), FfeeAccount.class);
+            if (owner == null) {
+                if (logger.isErrorEnabled()) {
+                    logger.error(String.format("The account[%s] not found.", course.getOwner().getId()));
+                }
+                throw new UserInterfaceFfeeErrorException(
+                        UserInterfaceFfeeErrorException.FfeeErrors.ACCOUNT_NOT_EXISTED
+                );
+            }
+        }
         if (!StringUtils.isBlank(course.getCode())) {
             // 根据代码来判定科目是否已经存在
             Course checked = generalDictAccessor.getByCode(course.getCode(), Course.class);
-            if (checked != null && StringUtils.isBlank(course.getId())) {
+            if (checked != null && StringUtils.isBlank(course.getId()) && checked.isValid()) {
                 // 如果ID为空而checked不为null，表示新增了同样代码的科目
                 if (logger.isErrorEnabled()) {
                     logger.error(String.format("The course[%s] has existed, code: %s.",
@@ -70,12 +82,30 @@ public class CourseServiceImpl implements CourseService {
                 throw new UserInterfaceFfeeErrorException(
                         UserInterfaceFfeeErrorException.FfeeErrors.COURSE_HAS_EXISTED
                 );
-            } else if (checked != null && !StringUtils.isBlank(course.getId())) {
+            } else if (checked != null) {
                 // 修改科目
                 checked.setType(course.getType());
+                checked.setCode(course.getCode());
+                checked.setName(course.getName());
+                checked.setDesc(course.getDesc());
+                course = checked;
             }
+            course.setOwner(owner);
+            course.setValid(true);
+            course = generalDictAccessor.save(course);
+            if (logger.isDebugEnabled()) {
+                logger.debug(String.format("Save course[%s] successfully, code: %s, name: %s, desc: %s.",
+                        course.getId(), course.getCode(), course.getName(), course.getDesc()));
+            }
+            return course;
+        } else {
+            if (logger.isErrorEnabled()) {
+                logger.error("The course's code is blank.");
+            }
+            throw new UserInterfaceSystemErrorException(
+                    UserInterfaceSystemErrorException.SystemErrors.SYSTEM_ILLEGAL_PARAM
+            );
         }
-        return null;
     }
 
     @Transactional
